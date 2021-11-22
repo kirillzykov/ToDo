1fb726b94be644207d874b72dbd0a57835145dd5
Kirill Zykov <kirillzykov777@gmail.com>
2021-11-22 23:14:49 +0300

    Initial setup

---
+++ ToDo.Core.Entity/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ToDo.Core.Entity
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ ToDo.Core.Entity/Models/ToDo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Core.Entity.Models
+{
+    class ToDo : IEntity
+    {
+        public int Id { get; set; }
+        public string Task;
+        bool IsComplete;
+        DateTime Date;
+    }
+}
